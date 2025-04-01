8d2b48e63fb441ed57090886cb2b051f6b4a1978
woochang <show4031132@gmail.com>
2025-04-01 16:08:16 +0900

    [svelte-news] route 설정 및 News 페이지 구성완료료

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script>
-  import Home from "./lib/components/Home.svelte";
-  import About from "./lib/components/About.svelte";
+    import Router, { location, link } from "svelte-spa-router";
+    import Home from "./lib/components/Home.svelte";
+    import About from "./lib/components/About.svelte";
+    import News from "./lib/components/News.svelte";
 </script>
 
-<!-- <Home /> -->
- <About />
 
-<style>
+<Router routes={
+  {
+    '/' : Home,
+    '/about' : About,
+    '/news': News
+  }
+} 
+/>
 
-</style>
---
+++ src/lib/components/News.svelte
@@ -0,0 +1,56 @@
+<script>
+    import Header from "./Header.svelte";
+    import Footer from "./Footer.svelte";
+    import news from "../../assets/news";
+
+    let newsData = $state(news);
+</script>
+
+<Header />
+<main class="container">
+    <h2>비보이 브라더스 활동</h2>
+    <div class="news">
+        {#each newsData as news}
+        <div class="news-item">
+            <img src={news.imgUrl} alt="new1">
+            <div class="item-text">
+                <h3>{news.title}</h3>
+                <p class="date">{news.date}</p>
+                <div class="hashtag">
+                    {#each news.hashtag as tag}
+                    <span>#{tag}</span>
+                    {/each}
+                </div>
+            </div>
+        </div>
+        {/each}
+    </div>
+</main>
+<Footer />
+
+<style lang="scss">
+    .news{
+        display: flex;
+        gap: 20px;
+        .item-text {
+            padding: 20px;
+        }
+        .news-item{
+            width: calc(100% / 3);
+            img{
+            width: 100%;
+            }
+            box-shadow: 4px 4px 10px rgba(0,0,0,0.2);
+        }
+    
+        h3 {margin-bottom: 10px;}
+        p {margin-bottom: 4px;}
+        .date, .hashtag{
+            font-size: 15px;
+            color: #666;
+        }
+    }
+    .container {
+        background: #fff;
+    }
+</style>
